2906508953edeec2f856643b5d22d1048f50b13d
Marcelo Petry <marcelospetry@gmail.com>
2022-11-18 16:28:51 -0300

    ExperienceCard basic structure

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function ExperienceCard({}: Props) {
-  return <div>ExperienceCard</div>;
+  return (
+    <article>
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="bg-white w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        src="../logosExperience/conab-logo-vert.jpg"
+        alt="conab-logo"
+      />
+
+      <div>
+        <h4>IT Operations Assistant</h4>
+        <p>Conab</p>
+        <div>
+          {/* Tech Used */}
+          {/* Tech Used */}
+          {/* Tech Used */}
+        </div>
+        <p>Started work... - Ended...</p>
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </article>
+  );
 }
 
 export default ExperienceCard;
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -14,6 +14,7 @@ function WorkExperience({}: Props) {
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
+        <ExperienceCard />
       </div>
     </motion.div>
   );

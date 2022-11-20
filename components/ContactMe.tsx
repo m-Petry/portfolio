6d8b6170d2bd505fdc4c228ba8786c5c3212d2c0
Marcelo Petry <marcelospetry@gmail.com>
2022-11-20 18:06:04 -0300

    contacts ok (soh falta server integration)

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function ContactMe({}: Props) {
+  // initialization of the connecting of state from react hook form
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  //   what to do with the data when subbmit:
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:marcelospetry@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}, ${formData.message} (${formData.email})`;
+  };
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] indent-5 text-gray-500 text-2xl">
         Contact
       </h3>
-      <div className="flex flex-col space-y-10 mt-20">
+      <div className="flex flex-col space-y-10 mt-20 scale-90 md:scale-100">
         <h4 className="text-4xl font-semibold text-center">
-          I am <span className="text-lime-600">open for work</span>. <br />
+          I{"'"}m <span className="text-lime-600">open to work</span>. <br />
           <span className="decoration-[#F7AB0A]/50 underline">
             Get in touch:
           </span>
@@ -30,16 +45,39 @@ function ContactMe({}: Props) {
             <p className="text-2xl">Porto Alegre, RS - Brasil</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 w-full md:w-screen md:max-w-4xl md:px-20 mx-auto">
+        {/* connect form with react hook form (onSubmit) */}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-full md:w-screen md:max-w-4xl md:px-20 mx-auto"
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="E-mail" className="contactInput" type="text" />
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="E-mail"
+              className="contactInput"
+              type="email"
+            />
           </div>
-          <input placeholder="Subject" className="contactInput" type="text" />
-          <textarea placeholder="Message" className="contactInput" />
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
           <button
             type="submit"
-            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+            className="bg-[#F7AB0A] hover:bg-yellow-600 py-5 px-10 rounded-md text-black font-bold text-lg"
           >
             Submit
           </button>

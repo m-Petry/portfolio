4c3367bc33889a71ad3a012b585fc836538a3ece
Marcelo Petry <marcelospetry@gmail.com>
2022-11-20 14:56:42 -0300

    projects page fully configurated

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -15,15 +16,29 @@ function Projects({}: Props) {
         {projects.map((project, i) => (
           <>
             <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44">
-              <img
+              <motion.img
+                initial={{
+                  y: -300,
+                  opacity: 0
+                }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
                 src="../imgsProject/89367.jpg"
-                alt=""
+                alt="webapps on all responsive devices"
                 className="rounded-lg"
               />
-              <div>
-                <h4>
-                  Case Study {i + 1} of {projects.length} fullstack eCommerce
+              <div className="space-y-10 px-0 md:px-10 max-w-6xl ">
+                <h4 className="text-4xl font-semibold text-center">
+                  <span className="underline decoration-[#F7AB0A]/50">
+                    Case Study {i + 1} of {projects.length}:
+                  </span>{" "}
+                  fullstack eCommerce
                 </h4>
+                <p className="text-lg text-center md:text-left">
+                  Built with React 18, Material UI, Redux, Formik, Yup, Tailwind
+                  CSS & Stripe for back-end and payment...
+                </p>
               </div>
             </div>
           </>

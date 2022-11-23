c6f30d58f7abd0030f56e7c60ce76ea76ef16c24
Marcelo Petry <marcelospetry@gmail.com>
2022-11-23 12:10:29 -0300

    projects alignment done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,7 +15,7 @@ function Projects({ projects }: Props) {
         Projects
       </h3>
 
-      <div className="relative w-full p-10 max-w-5xl flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="relative w-full p-10 md:p-0 max-w-5xl flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projects?.map((project, i) => (
           <div
             key={project._id}
